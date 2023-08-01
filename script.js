795773f385d703dbb7baa36582c4cb83cc411e6a
Gabriela Arruda <gabrielacosta460@hotmail.com>
2023-07-31 21:50:38 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Tópicos em aula:
+// Desenvolver calculadora de Idade, utilizando template disponibilizado
+// Algoritmo:
+
+// 1. Pegar os valores
+// 2. Calcular a Idade
+   //   a. Com base no ano
+     // b. Com mês (EXTRA)
+     // c. Com dia (EXTRA)
+
+// 3. Gerar a faixa etária
+   
+   //Resultado            Faixa
+    // 0 à 12                Criança
+    // 13 à 17                Adolescente
+    // 18 à 65               Adulto
+    // Acima de 65         Idoso
+   
+
+// 4. Organizar o objeto pessoa para salvar na lista
+// 5. Cadastrar a pessoa na lista
+// 6. Função para carregar as pessoas, carrega a lista do localStorage, chamar ao carregar a página
+// 7. Renderizar o conteúdo da tabela com as pessoas cadastradas
+// 8. Botão para limpar os registros;
+
+
+
 function calcular(event) {
     event.preventDefault()
 
@@ -14,9 +41,9 @@ function calcular(event) {
 
 function receberValores() {
     let nomeRecebido = document.getElementById("nome").value.trim()
-    let diaRecebido = document.getDateElementById("dia").value
-    let mesdiaRecebido = document.getMonthElementById("mes").value
-    let anodiaRecebido = document.getFullYearElementById("ano").value
+    let diaRecebido = document.getElementById("dia").value
+    let mesdiaRecebido = document.getElementById("mes").value
+    let anodiaRecebido = document.getElementById("ano").value
 
 
 
@@ -33,15 +60,35 @@ function receberValores() {
 }
 
 function calcularidade(imc) {
-    const today = new Date();
-    const birthDate = new Date(dateString);
-    let idade = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-    
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    
-    return age;
+    let dataAtual = new Date();
+    let anoAtual= dataAtual.getFullYear();
+
+    let idade = anoAtual - diaRecebido
+    return idade
 }
-console.log(getAge("1991/05/02")) // 31
+
+function faixaetaria(idade) {
+    if(idade >= 0 && idade <= 12){
+       console.log("Criança")
+       return "Criança"
+  
+    } else if(idade >= 13 && idade <= 17) {
+       console.log("Adolescente")
+         return "Adolescente"
+    } else if(idade >= 18 && idade <= 65) {
+            console.log("Adulto")
+              return "Adulto"
+    } else if(idade > 65) {
+            console.log("Idoso")
+             return "Idoso"
+           }
+    }
+
+    function calcular (event) {
+        event.preventDefault()
+        
+    console.log ("Executada a função calcular.")
+        let usuario = faixaetaria()     
+        
+
+    }
